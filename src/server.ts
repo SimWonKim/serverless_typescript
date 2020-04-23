5b330ea2159e8e7d0b295c96f7f7bfad642c1fa6
KimSimWon <asdfg4421@gmail.com>
2020-04-24 04:30:22 +0900

    오버워치 리거 정보 조회 api 추가.
    skip limit 옵션이 따로 없어서 full로 가져옴.

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,44 @@
 import { Handler, Context } from "aws-lambda";
+import axios from "axios";
 
 interface Response {
-  header: Object;
-  statusCode: Number;
-  body: String;
+    header: Object;
+    statusCode: Number;
+    body: String;
 }
 
 const createResponse = (statusCode: Number, body: Object) => {
-  return {
-    header: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    },
-    statusCode,
-    body: JSON.stringify(body),
-  };
+    return {
+        header: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        },
+        statusCode,
+        body: JSON.stringify(body),
+    };
 };
 
 const hello: Handler = async (event: any, context: Context) => {
-  const response: Response = createResponse(200, { message: "hello" });
-  return response;
+    const response: Response = createResponse(200, { message: "hello" });
+    return response;
 };
 
-export { hello };
+const getAllLeaguers = async (event: any) => {
+    try {
+        const url = `${process.env.END_POINT}/players?locale=ko-kr`;
+        const request = await axios.get(url);
+        const leaguers: Array<Object> = request.data.content;
+
+        const response: Response = createResponse(200, { leaguers });
+        return response;
+    } catch (err) {
+        console.log(err);
+
+        const errObj = { message: err.message };
+
+        const response: Response = createResponse(500, errObj);
+        return response;
+    }
+};
+
+export { hello, getAllLeaguers };
